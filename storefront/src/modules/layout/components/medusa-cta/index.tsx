a7bb7f3c563759bb0731f5a19a6e02b65df16401
Seyhan <88387474+seyhanazak@users.noreply.github.com>
2025-04-26 09:21:04 +0000

    Text edits

--- storefront/src/modules/layout/components/medusa-cta/index.tsx
+++ storefront/src/modules/layout/components/medusa-cta/index.tsx
@@ -5,6 +5,7 @@ import NextJs from "../../../common/icons/nextjs"
 
 const MedusaCTA = () => {
   return (
+    /*
     <Text className="flex gap-x-2 txt-compact-small-plus items-center">
       Powered by
       <a href="https://www.medusajs.com" target="_blank" rel="noreferrer">
@@ -15,6 +16,7 @@ const MedusaCTA = () => {
         <NextJs fill="#9ca3af" />
       </a>
     </Text>
+    */
   )
 }
 
--- storefront/src/modules/layout/templates/footer/index.tsx
+++ storefront/src/modules/layout/templates/footer/index.tsx
@@ -18,7 +18,7 @@ export default async function Footer() {
               href="/"
               className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base uppercase"
             >
-              Medusa Store
+              PAQ
             </LocalizedClientLink>
           </div>
           <div className="text-small-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
@@ -107,7 +107,7 @@ export default async function Footer() {
               </div>
             )}
             <div className="flex flex-col gap-y-2">
-              <span className="txt-small-plus txt-ui-fg-base">Medusa</span>
+              <span className="txt-small-plus txt-ui-fg-base">PAQ</span>
               <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
                 <li>
                   <a
@@ -116,7 +116,7 @@ export default async function Footer() {
                     rel="noreferrer"
                     className="hover:text-ui-fg-base"
                   >
-                    GitHub
+                    About
                   </a>
                 </li>
                 <li>
@@ -126,7 +126,7 @@ export default async function Footer() {
                     rel="noreferrer"
                     className="hover:text-ui-fg-base"
                   >
-                    Documentation
+                    Help / FAQs
                   </a>
                 </li>
                 <li>
@@ -136,7 +136,7 @@ export default async function Footer() {
                     rel="noreferrer"
                     className="hover:text-ui-fg-base"
                   >
-                    Source code
+                    Contact Support
                   </a>
                 </li>
               </ul>
@@ -145,7 +145,7 @@ export default async function Footer() {
         </div>
         <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
           <Text className="txt-compact-small">
-            © {new Date().getFullYear()} Medusa Store. All rights reserved.
+            © {new Date().getFullYear()} PAQ. All rights reserved.
           </Text>
           <MedusaCTA />
         </div>
